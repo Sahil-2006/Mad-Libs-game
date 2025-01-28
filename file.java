21b91451fd62e7a7bb25093df646c579e66fa60b
Sahil Shabbirali Haryani <137910815+Sahil-2006@users.noreply.github.com>
2025-01-28 16:14:49 +0000

    Created the necessary variables and taken input from the user

--- file.java
+++ file.java
@@ -1,6 +1,22 @@
 import java.util.Scanner;
 public class file {
     public static void main(String[] args) {
-        System.out.print("Hello world");
+        Scanner scanner = new Scanner(System.in);
+        String adjective1;
+        String noun1;
+        String adjective2;
+        String verb1;
+        String adjective3;
+        System.out.print("Enter an adjective(description): ");
+        adjective1 = scanner.nextLine;
+        System.out.print("Enter a noun(animal or person): ");
+        noun1 = scanner.nextLine;
+        System.out.print("Enter an adjective(description): ");
+        adjective2 = scanner.nextLine;
+        System.out.print("Enter a verb ending with -ing(action): ");
+        verb1 = scanner.nextLine;
+        System.out.print("Enter an adjective (description): ");
+        adjective3 = scanner.nextLine;
+        scanner.close();
     }
 }
