158ad7304d85be613291d0c5432e6491f8f382d2
Sahil Shabbirali Haryani <137910815+Sahil-2006@users.noreply.github.com>
2025-01-28 16:28:09 +0000

    Completed Mad libs game

--- file.java
+++ file.java
@@ -8,15 +8,18 @@ public class file {
         String verb1;
         String adjective3;
         System.out.print("Enter an adjective(description): ");
-        adjective1 = scanner.nextLine;
+        adjective1 = scanner.nextLine();
         System.out.print("Enter a noun(animal or person): ");
-        noun1 = scanner.nextLine;
+        noun1 = scanner.nextLine();
         System.out.print("Enter an adjective(description): ");
-        adjective2 = scanner.nextLine;
-        System.out.print("Enter a verb ending with -ing(action): ");
-        verb1 = scanner.nextLine;
+        adjective2 = scanner.nextLine();
+        System.out.print("Enter a verb (action): ");
+        verb1 = scanner.nextLine();
         System.out.print("Enter an adjective (description): ");
-        adjective3 = scanner.nextLine;
+        adjective3 = scanner.nextLine();
         scanner.close();
+        System.out.println("One day, a " + adjective1 + " " + noun1 + " decided to " + verb1 + " in the middle of the street.");
+        System.out.println("It was so " + adjective2 + " that people couldn't stop staring, and it looked " + adjective3 + " while doing it.");
+        System.out.println("Eventually, it got tired and sat down, asking for a snack of " + adjective1 + " pizza. The end.");
     }
 }
